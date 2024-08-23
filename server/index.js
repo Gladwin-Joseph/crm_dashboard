00c1935c291c7c0b98c5048aedec727b38c34836
Gladwin Joseph <gladwinj@RPTECH.CO>
2024-08-23 15:25:32 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors= require("cors");
 
-app.use(cors());
+app.use(cors({ origin: 'https://crmroster.rptechindia.com' }));
 app.use(express.json());
 
 let tempDataStore = {};
